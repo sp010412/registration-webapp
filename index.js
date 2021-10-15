e6c971e8d30660ec898744310ea43b8307aaf45a
sp010412 <smajwede7@gmail.com>
2021-10-15 11:50:44 +0200

    Separating routs and code

--- index.js
+++ index.js
@@ -33,8 +33,6 @@ const pool = new Pool({
     }
 });
 
-// const Routes = routes(greetInsta)
-
 app.engine('handlebars', handlebarSetup);
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
@@ -53,83 +51,18 @@ app.use(bodyParser.json())
 
 
 const regInsta = registrationNumbers(pool);
+const Routes = routes(regInsta);
 
 
-app.get('/', async function (req, res) {
-    res.render('index',{
-        output: await regInsta.allRegistrations()
-    })
-});
+app.get('/', Routes.home);
 
-app.post('/reg_numbers', async function (req, res) {
+app.post('/reg_numbers', Routes.actions);
 
-    try {
+app.post('/showButton', Routes.show);
 
-        const regex = /^((CA|PA|WC)\s\d{3}\-\d{3})$|^((CA|PA|WC)\s\d{3}\s\d{3})$|^((CA|PA|WC)\s\d{4})$/;
-        var plateIn = req.body.inputBox;
+app.post('/showAllButton', Routes.showAll);
 
-        if (plateIn) {
-            var listDi = await regInsta.dupli(plateIn);
-            if (regex.test(plateIn) === false) {
-                req.flash('infoRed', 'Not a registration plate! eg; CA 12345 ');
-            }
-            else if (listDi.rowCount === 1) {
-                req.flash('infoRed', 'Registration already exists!');
-            }
-            else if (regex.test(plateIn)) {
-                req.flash('infoIn', 'Plate Added!');
-                await regInsta.platesIn(plateIn);
-            }
-        }
-        else if (plateIn === "") {
-            req.flash('infoRed', 'Enter a registration plate!');
-        }
-
-        res.render('index', {
-            output: await regInsta.allRegistrations()
-        }
-        );
-    } catch (err) {
-        console.log(err)
-    }
-
-});
-
-app.post('/showButton', async function (req, res) {
-    var showFromTown = req.body.slct;
-    var list = await regInsta.findFromTown(showFromTown);
-
-    if (list.length === 0 && showFromTown === "1" ) {
-        req.flash('infoRed', 'No registration plates from Cape Town!');
-    }
-    else if (list.length === 0 && showFromTown === "2") {
-        req.flash('infoRed', 'No registration plates from Pretoria!');
-    }
-    else if (list.length === 0 && showFromTown === "3") {
-        req.flash('infoRed', 'No registration plates from Worcester!');
-    }
-    else if (list.length === 0 && showFromTown === "0"){
-        req.flash('infoRed', 'Select prefered town!');
-    }
-
-
-    res.render('index', {
-        output: list
-    }
-    );
-});
-
-
-app.post('/showAllButton', async function (req, res) {
-    res.render('index', { output: await regInsta.allRegistrations() });
-});
-
-
-app.post('/resetButton', async function (req, res) {
-    req.flash('infoIn', 'Database is successfully cleared!');
-    await regInsta.clearTable();
-    res.redirect('/');
-});
+app.post('/resetButton', Routes.reset);
 
 
 let PORT = process.env.PORT || 2022;

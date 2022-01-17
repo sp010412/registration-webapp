c6fe2205e613596d63d9426a6015f6752ddff370
sp010412 <smajwede7gmail.com>
2022-01-17 20:38:47 +0200

    hi

--- public/css/timer.js
+++ public/css/timer.js
@@ -1,11 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
     let red = document.querySelector('.error');
-    let green = document.querySelector('.passed');
-
-    if (red.innerHTML !== '' || green) {
-        setTimeout(() => {
-            red.innerHTML = '';
-            green.innerHTML = '';
-        }, 3000);
-    }
+    git config credential.helper
 });
